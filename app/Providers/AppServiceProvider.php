da38da52faae0e253b7217e6d3f2976b5c3c9a27
Igor T <igor.talevski@gmail.com>
2024-11-13 23:25:10 +0100

    add backup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,42 @@
 
 namespace App\Providers;
 
+use Google\Client;
+use Google\Service\Drive;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Masbug\Flysystem\GoogleDriveAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private function loadGoogleStorageDriver(string $driverName = 'google') {
+        try {
+            Storage::extend($driverName, function($app, $config) {
+                $options = [];
+
+                if (!empty($config['teamDriveId'] ?? null)) {
+                    $options['teamDriveId'] = $config['teamDriveId'];
+                }
+
+                $client = new Client();
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $service = new Drive($client);
+                $adapter = new GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+                $driver = new Filesystem($adapter);
+
+                return new FilesystemAdapter($driver, $adapter);
+            });
+        } catch(\Exception $e) {
+            dd($e->getMessage());
+            // your exception handling logic
+        }
+    }
+
     /**
      * Register any application services.
      */
@@ -21,6 +53,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->loadGoogleStorageDriver();
         //
     }
 }
